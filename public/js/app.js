8d881dd9dfd771026d88df0c31e32c2cfc1d16bf
nitin-22 <57023064+nitin-22@users.noreply.github.com>
2020-06-21 20:05:56 +0530

    Setup app for Heroku

--- public/js/app.js
+++ public/js/app.js
@@ -22,7 +22,9 @@ weatherForm.addEventListener('submit', (event) =>{
     message1.textContent = 'Loading...'
     message2.textContent = ''
     
-    fetch('http://localhost:3000/weather?address='+location).then((response) =>{
+    // fetch('http://localhost:3000/weather?address='+location).then((response) =>{
+
+    fetch('/weather?address='+location).then((response) =>{
         response.json().then((data) => {
             if(data.error){
                 // console.log(data.error)
--- src/app.js
+++ src/app.js
@@ -6,6 +6,8 @@ const geocode = require('./utils/geocode')
 const forecast = require('./utils/forecast')
 
 const app = express()
+const port = process.env.PORT || 3000
+
 
 // Define paths for Express config
 const publicDirectoryPath = path.join(__dirname, '../public')
@@ -100,8 +102,8 @@ app.get('*', (req, res) => {
     })
 })
 
-app.listen(3000, () => {
-    console.log('Server is up on port 3000.')
+app.listen(port, () => {
+    console.log('Server is up on port port.')
 })
 
 //to start we can use the nodemon app.js so that all the changes are done simultaneously
